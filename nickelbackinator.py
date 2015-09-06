3da12df630e00a95c0592ab5c3d0abfdaad1a30d
Sean Fogle <sean.fogle@gmail.com>
2015-09-05 20:23:51 -0500

    define first route

---
+++ nickelbackinator.py
@@ -0,0 +1,19 @@
+from flask import Flask
+#from redis import Redis
+
+
+#redis = Redis()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Here be Nickelback'
+
+# @app.route('/random')
+# def random():
+#     nickelback = redis.srandmember("nickelbacks", 1)
+#     return nickelback.as_json()
+
+if __name__ == '__main__':
+    app.run()
